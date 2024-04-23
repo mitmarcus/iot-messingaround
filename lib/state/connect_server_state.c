a4bb7902302a83f66b1769c466560160a64260bb
Jan Metela <janmetela@seznam.cz>
2024-04-23 08:46:13 +0200

    IOT-1-2-2 replacing the hardcoded values

--- lib/state/connect_server_state.c
+++ lib/state/connect_server_state.c
@@ -8,6 +8,7 @@
 
 State connect_server_state_switch(char* ip, int port){
     wifi_init(NULL);
-    wifi_command_create_TCP_connection("192.168.28.1",18, NULL, NULL);
+    char buffer[100];
+    wifi_command_create_TCP_connection(ip,port, NULL, buffer);
     return WORKING_STATE;
 }
